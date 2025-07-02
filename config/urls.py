dac5d9a1a98be21a92b2514730dff3bff616bc43
eladgnr <eladgnr@gmail.com>
2025-07-02 09:49:49 +0300

    added few docstrings

--- config/urls.py
+++ config/urls.py
@@ -3,6 +3,11 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+""" URL configuration for the vacation site project.
+This module defines the URL patterns for the project, including admin, authentication,
+and app-specific URLs.
+"""
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
